50c913f07419c463a4762d0858313f3fe6791405
Subramanyam Rekhandar <53898299+RekhandarSubramanyam@users.noreply.github.com>
2022-04-08 10:14:00 +0530

    Update function.php
    added new column is 'id' in table

--- CURD_php/function.php
+++ CURD_php/function.php
@@ -1,25 +1,25 @@
 <?php
 include("config.php");
-  if(isset($_POST['submit']) {
-   
+  if(isset($_POST['submit']))
+    {
     $username = $_POST['username'];
     $password = $_POST['password'];
     $gender = $_POST['gender'];
     $email = $_POST['email'];
     $phoneCode = $_POST['phoneCode'];
     $phone = $_POST['phone'];
-
-    $result=mysqli_query($mysqli,"INSERT INTO register values('$username', '$password', '$gender', '$email', '$phoneCode','$phone')");
-   }
+    
+    $result=mysqli_query($mysqli,"INSERT INTO register values('$username', '$password', '$gender', '$email', '$phoneCode','$phone','')");
+  
     if($result)
     {
     header("Location:insert.php");
     } 
     else{
-        echo "failed"
+        echo "failed";
     }
    
-}
+    }
 
 
 
